bab794ed10dbed0d5cdc1ec8ff4ab8d9f407a0e8
unknown <ruanfinamor@gmail.com>
2024-01-04 18:57:52 -0300

    First Commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+import symbolOn from './assets/pictures/symbol-on.png'
+import symbolOff from './assets/pictures/symbol-off.png'
+
+
+export default function App() {
+  const isActive = true;
+  return (    
+    <View style={styles.container}>      
+      <Image source={isActive ? symbolOn : symbolOff}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
